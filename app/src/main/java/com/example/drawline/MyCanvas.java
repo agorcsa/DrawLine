ae8a694f801c1d1f2badf12b512fdf8218b42dca
andreeagorcsa <andreea.gorcsa@t-systems.com>
2019-09-22 22:10:28 +0200

    Draw Lines using Canvas and Paint

--- app/src/main/java/com/example/drawline/MyCanvas.java
+++ app/src/main/java/com/example/drawline/MyCanvas.java
@@ -30,6 +30,14 @@ public class MyCanvas extends View {
 
         drawVerticalLines(canvas);
         drawHorizontalLines(canvas);
+        drawDiagonalLineLeftRight(canvas);
+        drawDiagonalLineRightLeft(canvas);
+        drawHorizontalLine1(canvas);
+        drawHorizontalLine2(canvas);
+        drawHorizontalLine3(canvas);
+        drawVerticalLine1(canvas);
+        drawVerticalLine2(canvas);
+        drawVerticalLine3(canvas);
     }
 
     private  void  drawVerticalLines(Canvas canvas) {
@@ -40,5 +48,56 @@ public class MyCanvas extends View {
     private void drawHorizontalLines(Canvas canvas) {
         canvas.drawLine(0f, getHeight() * Y_PARTITION_RATIO, getWidth(), getHeight() * Y_PARTITION_RATIO, paint);
         canvas.drawLine(0f, getHeight() * (2 * Y_PARTITION_RATIO), getWidth(), getHeight() * (2 * Y_PARTITION_RATIO), paint);
+    }
+
+
+    private void drawDiagonalLineLeftRight(Canvas canvas) {
+        paint.setColor(ContextCompat.getColor(getContext(), R.color.colorAccent));
+        paint.setStrokeWidth(2);
+        canvas.drawLine(0, 0, getWidth(), getHeight(), paint);
+    }
+
+    private void drawDiagonalLineRightLeft(Canvas canvas) {
+        paint.setColor(ContextCompat.getColor(getContext(), R.color.colorAccent));
+        paint.setStrokeWidth(2);
+        canvas.drawLine(0, getHeight(), getWidth(), 0, paint);
+    }
+
+
+    private void drawHorizontalLine1(Canvas canvas) {
+        paint.setColor(ContextCompat.getColor(getContext(), R.color.colorPrimary));
+        paint.setStrokeWidth(2);
+        canvas.drawLine(0, getHeight()/6, getWidth(), getHeight()/6, paint);
+    }
+
+    private void drawHorizontalLine2(Canvas canvas) {
+        paint.setColor(ContextCompat.getColor(getContext(), R.color.colorPrimary));
+        paint.setStrokeWidth(2);
+        canvas.drawLine(getWidth()/2, 0, getWidth()/2, getHeight(), paint);
+    }
+
+    private void drawHorizontalLine3(Canvas canvas) {
+        paint.setColor(ContextCompat.getColor(getContext(), R.color.colorPrimary));
+        paint.setStrokeWidth(2);
+        canvas.drawLine(0, getHeight() * 5/6, getWidth(), getHeight() * 5/6, paint);
+    }
+
+
+    private void drawVerticalLine1(Canvas canvas) {
+        paint.setColor(ContextCompat.getColor(getContext(), R.color.colorPrimary));
+        paint.setStrokeWidth(2);
+        canvas.drawLine(getWidth()/6, 0,  getWidth()/6, getHeight(), paint);
+    }
+
+    private void drawVerticalLine2(Canvas canvas) {
+        paint.setColor(ContextCompat.getColor(getContext(), R.color.colorPrimary));
+        paint.setStrokeWidth(2);
+        canvas.drawLine(0, getHeight()/2, getWidth(), getHeight()/2, paint);
+    }
+
+    private void drawVerticalLine3(Canvas canvas) {
+        paint.setColor(ContextCompat.getColor(getContext(), R.color.colorPrimary));
+        paint.setStrokeWidth(2);
+        canvas.drawLine(getWidth() * 5/6, 0, getWidth() * 5/6, getHeight(), paint);
     }
 }
